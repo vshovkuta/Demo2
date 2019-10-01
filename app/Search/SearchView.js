6d42ed92c0e5bf2a52932bcd880af26f690ae7ff
Volodymyr Shovkuta <47761397+vshovkuta@users.noreply.github.com>
2019-10-01 11:51:54 +0300

    Update SearchView.js

--- app/Search/SearchView.js
+++ app/Search/SearchView.js
@@ -2,10 +2,10 @@ export class SearchView {
   constructor(controller) {
     this.controller = controller;
     this.search = document.getElementById('search');
-    this.search.addEventListener('keyup', () => this.viewFound());
+    this.search.addEventListener('keyup', () => this.render());
   }
 
-  viewFound() {
+  render() {
     this.controller.controllers.product.updateRender(this.controller.getFindedObject(this.search.value.toLowerCase()));
   }
 
